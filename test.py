a71935fe2c488419a1525274721be5c4f07aaddb
GH0ST555 <110325018+GH0ST555@users.noreply.github.com>
2023-01-02 06:17:57 +0000

    updated the unittests making them more informative

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import os
 import unittest
+import json
 from flask import Flask
 from app import app
 
+tdata = None
+with open("app/static/adress.json", "r+") as f:
+   tdata=json.load(f)
+   f.close()
 
-def add(x,y):
-    return x + y
 
 class TestCase(unittest.TestCase):
+
+
     def setUp(self):
         app.config.from_object('config')
         app.config['TESTING'] = True
@@ -19,16 +24,35 @@ class TestCase(unittest.TestCase):
 
     def test_view_correct_record(self):
        response = self.app.get('/view?id=2',follow_redirects=True)
+       print('Testing To see if a record can be viewved')
+       print('expected Request : 200')
+       print('request recieved: ' ,response.status_code)
+       print('Sucessfuly Viewed')
        self.assertEqual(response.status_code, 200)
 
     def test_view_incorrect_record(self):
        response = self.app.get('/view?id=500')
+       print('Testing To see if an invalid record can be viewed')
+       print('expected Request : 404')
+       print('request recieved: ' ,response.status_code)
+       print('Hence test concludes that a random record cannot be viewed')
        self.assertEqual(response.status_code, 404)
 
 
     def test_create_user_page(self):
        response = self.app.get('/create_user',follow_redirects=True)
+       response2 = self.app.post('/create_user',data ={"email": "aktest", "fname": "test",  "lname": "er","pno": "123456"},follow_redirects=True)
+       print('Testing To see if the record form can be seen')
+       print('expected Request : 200')
+       print('request recieved: ' ,response.status_code)
+       print('Hence test concludes that the homepage can be viewed')
+       print('\n') 
+       print('Testing To see if a record can be created on sucess it should return to homepage')
+       print('expected Request : /')
+       print('request recieved: ' ,response.request.path)
+       print('Hence test concludes that the homepage can be viewed')        
        self.assertEqual(response.status_code, 200)
+       self.assertEqual(response2.request.path, '/')
 
     def test_correct_edit(self):
        response = self.app.get('/edit/2',follow_redirects=True)
@@ -36,16 +60,26 @@ class TestCase(unittest.TestCase):
     
     def test_incorrect_edit(self):
        response = self.app.get('/edit/500')
+
+       print('Testing To see if the Record can be accessed for editing')
+       print('expected Request : 404')
+       print('request recieved: ' ,response.status_code)
+       print('Error as The record cannot be accessed')
        self.assertEqual(response.status_code, 404)
 
     def test_addtaskroute(self):
        response = self.app.get('/',follow_redirects=True)
+       print('Testing To see if Homepage can be viewed')
+       print('expected Request : 200')
+       print('request recieved: ' ,response.status_code)
+       print('Hence test concludes that the homepage can be viewed')
        self.assertEqual(response.status_code, 200)
+   
 
     def tearDown(self):
+        with open("app/static/adress.json", "w") as outfile:
+            json.dump(tdata, outfile, indent=4,  separators=(',',': '))
+            outfile.close()
         pass
-        # db.session.remove()
-        # db.drop_all()   
-
 if __name__ == '__main__':
     unittest.main()
